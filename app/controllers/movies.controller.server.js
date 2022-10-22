54e1d444758de0b201403967194be17687462a3a
Wilson Wu <wuwilson151@gmail.com>
2022-10-21 20:48:55 -0400

    commit

--- app/controllers/movies.controller.server.js
+++ app/controllers/movies.controller.server.js
@@ -14,3 +14,22 @@ export function DisplayMovieList(req,res,next){
 export function DisplayMoviesAddPage(req,res,next){
     res.render('index', { title: 'Add Movie', page: 'movies/edit', movies: {} })
 }
+
+export function ProcessMoviesAddPage(req,res,next) {
+    let newMovie = movieModel({
+        name: req.body.name,
+        year: req.body.year,
+        director: req.body.director,
+        genre: req.body.genre,
+        runtime: req.body.runtime
+    });
+
+    movieModel.create(newMovie, (err, Movie) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/movie-list')
+    })
+}
--- app/routes/movies.route.server.js
+++ app/routes/movies.route.server.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { DisplayMovieList, DisplayMoviesAddPage } from "../controllers/movies.controller.server.js";
+import { DisplayMovieList, DisplayMoviesAddPage, ProcessMoviesAddPage } from "../controllers/movies.controller.server.js";
 
 const router = Router();
 
 router.get('/movie-list', DisplayMovieList);
 router.get('/movie-add', DisplayMoviesAddPage);
+router.post('/movie-add', ProcessMoviesAddPage);
 
 export default router;
